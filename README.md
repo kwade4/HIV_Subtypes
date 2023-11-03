fd244048529462bde78bff0a08b2df32f41b2dd5
KaitlynWade <kwade4@uwo.ca>
2023-11-03 10:55:19 -0400

    -Updated preprocessing script -Added READ.ME

---
+++ README.md
@@ -0,0 +1,95 @@
+## HIV-1 Subtype Classification 
+
+####Background 
+
+* HIV-1 is classified into 4 groups: M, N, O, and P, and of these groups, group M is the most widespread and clinically relevant. 
+* Group M is subdivided into 9 distinct subtypes: A, B, C, D, F, G, H, J, K. 
+* 2 or more subtypes can combine to form a hybrid form called a circulating recombinant form (CRFs). 
+* Rates of disease progression vary among HIV-1 subtypes, with some subtypes showing increased drug resistance and virulence. 
+* Understanding the distribution of HIV-1 subtypes is crucial for the development of vaccines and the clinical management of HIV-1 infections.  For infected individuals, classifying HIV-1 subtype is a crucial step in clinical infection management.  
+* HIV-1 subtyping = classifying HIV-1 into subtypes. 
+
+#### Research Gap 
+* Most HIV-1 subtype classification methods rely on aligning an input sequence against a set of pre-defined subtype reference sequences. These alignment-based methods are computationally expensive, especially for long sequences and rely on ad hoc parameter settings, which can limit reproducibility. 
+* To address the limitations of alignment-based methods, alignment-free methods have been developed. 
+* Few studies (if any) have compared DNA vectorization methods for HIV-1 subtyping. 
+* Few studies (if any) have applied multi-task learning to HIV-1 subtype classification. 
+
+#### Possible Research Objectives 
+* To characterize the effect of DNA sequence vectorization methods on HIV-1 subtyping. 
+* Apply multi-task learning to HIV-1 subtype classification. 
+* Develop an improved HIV-1 subtype classification method. 
+
+#### Methodology 
+
+1. Explore different ways to vectorize DNA sequences: 
+    * **Word2Vec** 
+    * **K-mers** (of varying length)
+    * **Subsequence natural vector**: includes number and distrubution of nucleotides, position, and variance 
+    * **Natural vector**: number and distribtion of nucleotides
+    * Other language model encodings
+ 
+2. Feature selection:
+    * LASSO
+    * PCA
+    * Pre-trained CNN (as Kyle suggested)  
+
+3. Machine Learning & Deep Learning  
+    * Classical Machine Learning 
+        * SVM (one-vs-rest)
+        * Multi-class logistic regression 
+        * XGBoost
+        * LASSO
+        * Naive Bayes?  
+        * KNN clustering 
+    * Deep Learning 
+        * 1D-CNN 
+    * Multi-task learning (time-permitting)
+    
+4. Evaluation Metrics 
+    * Confusion Matrix
+    * Accuracy, precision (macro-precision), recall, F1-score (macro F1-score)
+    * AUROC, AUPRC
+    * Cohen's Kappa 
+    
+At our meeting, we had discussed evaluating our models with and without applying feature selection. 
+
+
+#### Distribution of Tasks 
+
+#####Experiments 
+* Processing data - Kaitlyn  
+* Vectorize DNA Sequences** - Gen and Kaitlyn 
+    * Kaitlyn: k-mers, natural (and sub-sequence) vectors
+    * Gen: Word2Vec, other natural language encoding methods 
+* Feature Selection: Alana 
+* Machine Learning and Deep Learning: Kyle 
+* Multi-task Learning - ? 
+
+#####Paper
+* Background and introduction: Kaitlyn 
+* Methods 
+    * Data-processing: Kaitlyn 
+    * Sequence vectorization: Kaitlyn and Gen 
+    * Feature Selection: Alana 
+    * ML/DL: Kyle 
+* Results and Discussion: Kyle 
+* Figures: Kyle  
+
+#### Dataset and Availability 
+
+#####Dataset 
+* `hiv-db.tar.xz`: contains 20,386 unprocessed sequences from 289 subtypes [LANL HIV Sequence Database](https://www.hiv.lanl.gov/components/sequence/HIV/search/search.html)
+* After processing there are 15,018 sequences and 28 subtypes. 
+* Each sequence is approx. 10,000 nucleotides.
+* `hiv.txt`: the sequences (atgcgctagatcga) 
+    * Each line corresponds to one sequence
+* `labels.txt`: the subtype label 
+    * The first line in `labels.txt` corresponds to the first sequence (line) in `hiv.txt`. 
+
+#####Scripts 
+* `pre-process.py`: reads `hiv-db.tar.xz`
+    * Removes sequences with unknown characters (eg: N) and subtypes with too few examples 
+    * Creates `hiv.txt` and `labels.txt`
+
+`hiv.txt` can be used to vectorize the sequences.  
--- pre-process.py
+++ pre-process.py
@@ -1,30 +1,99 @@
+"""
+This script processes HIV sequences downloaded from the HIV Sequences Database at the Los Alamos National Laboratory.
+"""
+
 from Bio import SeqIO
 
-infile = './hiv-db.fasta'
-fasta_sequences = SeqIO.parse(open(infile), 'fasta')
+DNA_ALPHABET = 'atgc'
 
-subtype_counts = {}
 
-# with open('./subtype-accession.txt', 'w') as outfile:
-#     for fasta in fasta_sequences:
-#         name, sequence = fasta.id, str(fasta.seq)
-#         outfile.write('{}\n'.format(name))
+def get_subtype_seqs(seqs):
+    """
+    Organize subtype sequences into a dictionary
+    :param seqs: a list of FASTA sequences
+    :return: a dictionary of sequences where each
+    """
+    count = 0
+    raw_subtype_seqs = {}
+    for fasta in seqs:
+        subtype = fasta.id.split('.')[0]
+        if subtype not in raw_subtype_seqs:
+            raw_subtype_seqs[subtype] = []
+        raw_subtype_seqs[subtype].append(str(fasta.seq))
+        count +=1
 
-for fasta in fasta_sequences:
-    name, sequence = fasta.id, str(fasta.seq)
-    subtype_name = name.split('.')[0]
+    print('There are {} sequences before processing.'.format(count))
+    print('There are {} subtypes before processing.'.format(len(raw_subtype_seqs.keys())))
+    return raw_subtype_seqs
 
-    if subtype_name not in subtype_counts:
-        subtype_counts[subtype_name] = 0
-    subtype_counts[subtype_name] += 1
 
-# 289 total subtypes, including one unknown
-clean_subtype_counts = subtype_counts.copy()
-for st in subtype_counts:
-    # Set arbitrary threshold for cut-off
-    if subtype_counts[st] < 18:
-        del clean_subtype_counts[st]
+def process_sequences(subtype_seqs, threshold=18):
+    """
+    Process the HIV sequences by removing subtypes with counts below a given threshold.
+    :param subtype_seqs: dictionary of sequences, grouped by subtypes
+    :param threshold: minimum number of sequences for a subtype, as per Solis-Reyes et al., 2018
+    :return: a dictionary of counts for each subtype, with low frequency subtypes removed
+    """
 
-# 37 subtypes (min count=21)
-print(len(clean_subtype_counts))
-print('\n'.join('{} {}'.format(k, int(v)) for k, v in clean_subtype_counts.items()))
+    # Remove subtypes that have too few examples, as defined by the threshold
+    for subtype in subtype_seqs.copy():
+
+        # Check that all sequences contain only 'a', 't', 'g', or 'c'
+        seq_list = subtype_seqs[subtype]
+        clean_seq_list = []
+        for seq in seq_list:
+            if all(nt in DNA_ALPHABET for nt in seq):
+                clean_seq_list.append(seq)
+        subtype_seqs[subtype] = clean_seq_list    # Include only sequences that contain 'a', 't', 'g', or 'c'
+
+        # Remove sequences with too few examples
+        count = len(subtype_seqs[subtype])
+        if count < threshold:
+            del subtype_seqs[subtype]
+
+    print('There are {} subtypes after processing.'.format(len(subtype_seqs)))
+    return subtype_seqs
+
+
+def get_subtype_counts(subtype_seqs, outfile='./subtype-counts.txt'):
+    """
+    Writes the counts for each subtype to a file
+    :param subtype_seqs:
+    :param outfile: path to the output file
+                    By default, it will create a file in the working directory called 'raw-subtype-counts.txt'
+    """
+    with open(outfile, 'w+') as out_handle:
+        for subtype in subtype_seqs:
+            out_handle.write('{} {}\n'.format(subtype, len(subtype_seqs[subtype])))
+    return
+
+
+def write_clean_data(subtype_seqs, seq_outfile='./hiv.txt', label_outfile='./label.txt'):
+    """
+    Write DNA sequences to a file
+    :param subtype_seqs: dictionary of sequences
+    :param seq_outfile: output file for sequences
+    :param label_outfile: output file for labels
+    """
+    with open(seq_outfile, 'w+') as seq_handle, open(label_outfile, 'w+') as label_handle:
+        for subtype in subtype_seqs:
+            for seq in subtype_seqs[subtype]:
+                seq_handle.write('{}\n'.format(seq))
+                label_handle.write('{}\n'.format(subtype))
+    return
+
+
+if __name__ == '__main__':
+
+    # Read in and parse sequences
+    infile = './hiv-db.fasta'
+    fasta_sequences = list(SeqIO.parse(open(infile), 'fasta'))
+    raw_subtype_seqs = get_subtype_seqs(fasta_sequences)
+
+    # Remove subtypes with too few examples
+    subtype_seqs = process_sequences(raw_subtype_seqs)
+    get_subtype_counts(subtype_seqs)
+
+    # Write sequences and labels to output files
+    # The first line in hiv.txt corresponds to the first sequence and the label/ subtype is the first line in label.txt
+    write_clean_data(subtype_seqs)
